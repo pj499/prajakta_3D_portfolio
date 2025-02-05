fe28e49d0cd061f34fd8efea4a1853e6f4e2b609
Prajakta Pikale <pjprajakta499@gmail.com>
2025-02-05 15:52:44 -0600

    setup components skeleton and assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import Navbar from './sections/Navbar.jsx';
+
 
 const App = () => {
   return (
     <main className="max-w-7xl mx-auto">
       <h1 className="text-2xl text-white underline">Hello, Three.js!</h1>
-      <Navbar/>
     </main>
   );
 };
