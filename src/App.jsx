ad17c5dda2c9816dbe41898c5a9e86cd8dc1a4eb
Prajakta Pikale <pjprajakta499@gmail.com>
2025-02-05 17:27:35 -0600

    navbar elelememts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components';
 
 const App = () => {
   return (
-    <main className="max-w-7xl mx-auto">
-      <h1 className="text-2xl text-white underline">Hello, Three.js!</h1>
-    </main>
+   <BrowserRouter>
+     <div className="relative z-0 bg-primary">
+       <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+         <Navbar />
+         <Hero/>
+       </div>
+       <About />
+       <Experience />
+       <Tech />
+       <Works />
+       <Feedbacks />
+       <div className="relative z-0">
+         <Contact />
+         <StarsCanvas />
+       </div>
+     </div>
+   </BrowserRouter>
   );
 };
 
