cff3f55bfc276b3838bebdd0310590fbce26f945
Prajakta Pikale <pjprajakta499@gmail.com>
2025-02-06 21:06:12 -0600

    testing fixes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,22 +39,22 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
-        <a href="#about">
-          <div className="w-[35px] h-[64px] rounded-3xl border-2 border-secondary flex justify-center items-start p-2 relative">
-            <motion.dev
-              animate={{ y: [0, 20, 0] }}
-              transition={{ duration: 3, repeat: Infinity, repeatType: 'loop' }}
-              className="w-0.5 h-5 bg-secondary mb-1"
-            />
-            <motion.dev
-              animate={{ y: [0, 20, 0] }}
-              transition={{ duration: 3, repeat: Infinity, repeatType: 'loop' }}
-              className="w-2 h-2 rounded-full bg-secondary mb-1 absolute top-6"
-            />
-          </div>
-        </a>
-      </div>
+      {/*<div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">*/}
+      {/*  <a href="#about">*/}
+      {/*    <div className="w-[35px] h-[64px] rounded-3xl border-2 border-secondary flex justify-center items-start p-2 relative">*/}
+      {/*      <motion.dev*/}
+      {/*        animate={{ y: [0, 20, 0] }}*/}
+      {/*        transition={{ duration: 3, repeat: Infinity, repeatType: 'loop' }}*/}
+      {/*        className="w-0.5 h-5 bg-secondary mb-1"*/}
+      {/*      />*/}
+      {/*      <motion.dev*/}
+      {/*        animate={{ y: [0, 20, 0] }}*/}
+      {/*        transition={{ duration: 3, repeat: Infinity, repeatType: 'loop' }}*/}
+      {/*        className="w-2 h-2 rounded-full bg-secondary mb-1 absolute top-6"*/}
+      {/*      />*/}
+      {/*    </div>*/}
+      {/*  </a>*/}
+      {/*</div>*/}
     </section>
   );
 }
